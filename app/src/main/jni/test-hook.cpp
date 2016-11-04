6ec81928a0cc2c52ca27685589b24415a0b229d6
jalen <962076917@qq.com>
2016-11-04 10:48:43 +0800

    substrate native库导入

--- app/src/main/jni/test-hook.cpp
+++ app/src/main/jni/test-hook.cpp
@@ -27,28 +27,24 @@ static jint $Resources$getColor(JNIEnv *jni, jobject _this, jint rid) {
 
 int (*open)(const char*, int, ...); // 保留原来的地址（就是MSHookFunction调用后会把原来的地址保存在了这里）
 int hookopen(const char* pathname, int flags, ...){
-    char* su_1 = "/system/app/Superuser.apk";
-    char* su_2 = "/sbin/su";
-    char* su_3 = "/system/bin/su";
-    char* su_4 = "/system/xbin/su";
-    char* su_5 = "/data/local/xbin/su";
-    char* su_6 = "/data/local/bin/su";
-    char* su_7 = "/system/sd/xbin/su";
-    char* su_8 = "/system/bin/failsafe/su";
-    char* su_9 = "/data/local/su";
-    char* su_0 = "/su/bin/su";
-    if (strcasecmp(pathname, su_0)){
+    char *su_1 = "/system/app/Superuser.apk";
+    char *su_2 = "/sbin/su";
+    char *su_3 = "/system/bin/su";
+    char *su_4 = "/system/xbin/su";
+    char *su_5 = "/data/local/xbin/su";
+    char *su_6 = "/data/local/bin/su";
+    char *su_7 = "/system/sd/xbin/su";
+    char *su_8 = "/system/bin/failsafe/su";
+    char *su_9 = "/data/local/su";
+    char *su_0 = "/su/bin/su";
+    if (strcasecmp(pathname, su_0) || strcasecmp(pathname, su_1) || strcasecmp(pathname, su_2) || strcasecmp(pathname, su_3)
+            || strcasecmp(pathname, su_4) || strcasecmp(pathname, su_5) || strcasecmp(pathname, su_6)
+            || strcasecmp(pathname, su_7) || strcasecmp(pathname, su_8) || strcasecmp(pathname, su_9)){
         return -1;
     }
     return open(pathname,flags);
 }
 
-static void OnResources(JNIEnv *jni, jclass resources, void *data) {
-    jmethodID method = jni->GetMethodID(resources, "getColor", "(I)I");
-    if (method != NULL)
-        MSJavaHookMethod(jni, resources, method,
-            &$Resources$getColor, &_Resources$getColor);
-}
 MSInitialize {
     LOGI("Substrate initialized.");
     MSImageRef image;
